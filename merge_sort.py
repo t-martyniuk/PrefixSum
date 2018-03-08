d6797d7d68d9f3355882053568fe03d5715d6c7c
t-martyniuk <tetyanka.martynyuk@gmail.com>
2018-03-09 01:56:46 +0200

    updated mergesort

--- merge_sort.py
+++ merge_sort.py
@@ -1,15 +1,23 @@
 import multiprocessing
-#[2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 7, 8, 8, 9]
 
-def mergesort(lst):
+def mergesort_parallel(lst):
     if len(lst) == 1:
         return lst
     else:
         m = int(len(lst)/2)
-        l = mergesort(lst[:m])
-        r = mergesort(lst[m:])
-        #return seq_merge(l,r,lst)
+        l = mergesort_parallel(lst[:m])
+        r = mergesort_parallel(lst[m:])
         return par_merge(l,r,lst)
+    
+def mergesort_sequential(lst):
+    if len(lst) == 1:
+        return lst
+    else:
+        m = int(len(lst)/2)
+        l = mergesort_sequential(lst[:m])
+        r = mergesort_sequential(lst[m:])
+        return seq_merge(l,r,lst)
+
 
 def seq_merge(l,r,lst):
     i = 0
@@ -36,7 +44,6 @@ def seq_merge(l,r,lst):
     return lst
 
 def par_merge(l,r,lst):
-
     for i in range(len(l)):
         lst[i + par_bin_search(l[i],r)] = l[i]
     for i in range(len(r)):
@@ -44,43 +51,57 @@ def par_merge(l,r,lst):
 
     return lst
 
+def seq_bin_search(item,alist):
+    first = 0
+    last = len(alist)-1
+    while first<=last:
+        midpoint = (first + last)//2
+        if item < alist[midpoint]:
+            last = midpoint - 1
+        else:
+            first = midpoint + 1
+    return first
+
 def par_bin_search(el, arr):
+
     max_cores = multiprocessing.cpu_count()
     jobs = []
-    processing_field = len(arr) // (max_cores + 1)
     bin_array = [0]*max_cores
-    idx_array = [0]*(max_cores+2)
-    idx_array[0] = 0
-    idx_array[-1] = len(arr)
+    idx_array = [0]*max_cores
 
-    def comparison(el, core, num_per_core):
-        right = arr[(core + 1) * num_per_core - 1]
-        idx_array[core+1] = right
-        if el >= right:
-            bin_array[core] = 0
-        else:
-            bin_array[core] = 1
-
-    for i in range(max_cores):
-        p = multiprocessing.Process(target=comparison, args=(el, i, processing_field))
-        p.daemon = False
-        jobs.append(p)
-        p.start()
-    for p in jobs:
-        p.join()
-    if bin_array[0] == 1:
-        par_bin_search(el, arr[:idx_array[0]+1])
-    elif bin_array[-1] == 0:
-        par_bin_search(el, arr[idx_array[-1]:])
+    if len(arr) < max_cores + 1:
+        idx = seq_bin_search(el,arr)
+        return idx
     else:
-        idx = 0
-        while (idx < max_cores) and (bin_array[idx] == 0):
-            idx += 1
-        if idx_array[idx - 1] + 1 < idx_array[idx]:
-            par_bin_search(el, arr[idx_array[idx-1]:idx_array[idx]+1])
+        processing_field = len(arr)//(max_cores + 1)
+        for core in range(max_cores):
+            idx_array[core] = (core + 1) * processing_field - 1
+
+        def comparison(el, core, processing_field):
+            right = (idx_array[core])
+            if el >= arr[right]:
+                bin_array[core] = 0
+            else:
+                bin_array[core] = 1
+
+        for i in range(max_cores):
+            p = multiprocessing.Process(target=comparison, args=(el, i, processing_field))
+            p.daemon = False
+            jobs.append(p)
+            p.start()
+        for p in jobs:
+            p.join()
+        if bin_array[0] == 1:
+            return par_bin_search(el, arr[:idx_array[0] + 1])
+        elif bin_array[-1] == 0:
+            return par_bin_search(el, arr[idx_array[-1]:])
         else:
-            return idx_array[idx-1]
+            idx = 0
+            while (idx < max_cores) and (bin_array[idx] == 0):
+                idx += 1
+            if idx_array[idx - 1] + 1 < idx_array[idx]:
+                par_bin_search(el, arr[idx_array[idx - 1]:idx_array[idx] + 1])
+            else:
+                return idx_array[idx - 1]
 
-
-print(mergesort([3,4,9,8,7,2,4,6,5,6,5,8,4,3,4,5]))
 
